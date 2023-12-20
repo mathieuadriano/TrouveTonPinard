f177fb217909c0a879c06da1aaf4094a98b7a777
mathieuadriano <mathieu.adriano@hotmail.fr>
2023-12-20 16:36:28 +0100

    Update Image Model

--- backend/wine/models.py
+++ backend/wine/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch.dispatcher import receiver
 
 from winery.models import *
 
@@ -28,6 +30,11 @@ class Wine(models.Model):
     creation_year = models.CharField(max_length=50, null=False, blank=False)
     volume = models.CharField(max_length=50, null=False, blank=False)
     weekly_wine = models.BooleanField(default=False, null=False, blank=False)
+    image = models.ImageField(blank=False, null=False)
  
     def __str__(self):
          return self.name
+    
+@receiver(post_delete, sender=Wine)
+def image_delete(sender, instance, **kwargs):
+    instance.image.delete(False)
